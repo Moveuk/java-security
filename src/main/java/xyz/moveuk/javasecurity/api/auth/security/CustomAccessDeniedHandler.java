b1a6a8e672358ff7031983a0442a01bceaf30d20
moveuk <v_donguk@naver.com>
2024-08-12 22:45:57 +0900

    Fix: Security Exception Handler 수정

--- src/main/java/xyz/moveuk/javasecurity/api/auth/security/CustomAccessDeniedHandler.java
+++ src/main/java/xyz/moveuk/javasecurity/api/auth/security/CustomAccessDeniedHandler.java
@@ -1,7 +1,6 @@
 package xyz.moveuk.javasecurity.api.auth.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
@@ -15,7 +14,7 @@ import java.io.IOException;
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
--- src/main/java/xyz/moveuk/javasecurity/api/auth/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/xyz/moveuk/javasecurity/api/auth/security/CustomAuthenticationEntryPoint.java
@@ -1,7 +1,6 @@
 package xyz.moveuk.javasecurity.api.auth.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
@@ -15,7 +14,7 @@ import java.io.IOException;
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
